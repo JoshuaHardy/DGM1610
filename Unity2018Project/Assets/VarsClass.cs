cb8f48a969faf75276c95286042ae773404519e5
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-09-03 15:59:13 -0600

    collision Basics WIP

---
+++ Unity2018Project/Assets/VarsClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VarsClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+        public float floatValue = 5.5f;
+        public int intvalue = 20;
+        public string stringValue = "Bob";
+        public int firepower;
+        public UnityEvent testEvent;
+
+        private void OnTriggerEnter(Collider other)
+    {
+        testEvent.Invoke();
+        print("Watch where you're going!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
