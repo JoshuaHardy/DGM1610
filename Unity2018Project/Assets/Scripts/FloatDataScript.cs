a561017b5f347efaf3b171bb0c031509d7709a03
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-09-10 16:37:23 -0600

    Health and PowerUps WIP
    Also, Particle trigger for mouse down is broken, I may need to go find that one later.

---
+++ Unity2018Project/Assets/Scripts/FloatDataScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatDataScript : ScriptableObject
+{
+    public float value = 1f;
+
+    public void UpdateValue(float amount)
+    {
+        value += amount; 
+        // alternative syntaax is: {value = value + amount;} but the given syntax does the same and is shorter
+    }
+}
