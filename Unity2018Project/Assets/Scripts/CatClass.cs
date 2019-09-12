d43fdca35ea5640c8a86e201c2c321b1b156ac31
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-09-12 15:53:56 -0600

    9/12/2019 Classwork
    Events and  Particles

---
+++ Unity2018Project/Assets/Scripts/CatClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CatClass : MonoBehaviour
+{
+    public UnityEvent catStuff;
+    public UnityEvent catStuffTwo;
+    private void OnMouseDown()
+    {
+        catStuff.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        catStuffTwo.Invoke();
+    }
+    
+}
---
+++ Unity2018Project/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDown;
+    public UnityEvent mouseUp;
+    private void OnMouseDown()
+    {
+        mouseDown.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUp.Invoke();
+    }
+    
+}
