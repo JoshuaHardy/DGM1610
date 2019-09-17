17512a98e6b8feff54c3685db8d85aeac51f3289
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-09-17 16:00:22 -0600

    Character Movement
    Double Jump WIP

---
+++ Unity2018Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float gravity = -3f;
+    public float jumpSpeed = 10f;
+    private Vector3 position;
+    public CharacterController controller;
+    private int jumpCount = 2;
+    
+    
+     void Update()
+    {
+       position.x = speed*Input.GetAxis("Horizontal");
+       position.y -= gravity;
+       
+       if (!controller.isGrounded)
+       {
+           position.y -= gravity;
+       }
+       
+       if (controller.isGrounded)
+       {
+           jumpCount=2;
+       }
+       
+       if (Input.GetButtonDown("Jump"))
+           //"Jump" Is Mapped to the space bar by default in unity
+       {
+           position.y = jumpSpeed;
+           jumpCount--;
+       }
+       controller.Move(position*Time.deltaTime);
+       
+    }
+}
