474da9565190f4646aa2f122633a67d4904f488e
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-09-21 20:43:00 -0600

    Blast Zone Script Attempt 01

---
+++ Unity2018Project/Assets/Scripts/HudDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HudDisplay : MonoBehaviour
+{
+    public Text textbox;
+   // public float
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox = GetComponent<Text>();
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        textbox.text = "Test"; 
+                       //+ health;
+ 
+       // if(health <= 0)
+       //     Destroy(gameObject);
+    }
+}
---
+++ Unity2018Project/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerDeath position.y <= -5);
+        {
+            //Kill Player Here
+        }
+    }
+}
