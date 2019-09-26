6f263c6b35f6d79e5c4461599532f98a78f3ca09
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-09-26 15:48:24 -0600

    Lecture on Functions

---
+++ Unity2018Project/Assets/Scripts/LectureFunctions.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class LectureFunctions : MonoBehaviour
+{
+    public string playerName;
+    private int myNumber;
+    public float speed = 5;
+    public GameObject player;
+    public Color playerColor = Color.red;
+
+
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        configGameObject();
+        //   print(message: WelcomePlayer());
+        //  myNumber = AddNumbers();
+    }
+
+    private string WelcomePlayer()
+    {
+        //Add player to database;
+        //Give Player Weapons
+        return playerName + "Welcome to the Game!";
+    }
+    private int AddNumbers(int a, int b)
+    {
+        print(a + b);
+        return a + b;
+    }
+
+    private GameObject configGameObject()
+    {
+        var newPlayer = Instantiate(player);
+        newPlayer.layer = 0;
+        newPlayer.active = true;
+        newPlayer.tag = "Player";
+        newPlayer.transform.position = Vector3.zero;
+        newPlayer.name = playerName;
+        newPlayer.GetComponent<Renderer>().material.color = playerColor;
+        
+        return newPlayer;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = IncreaseSpeed(2f);
+    }
+    
+    private float IncreaseSpeed(float multiplier)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
