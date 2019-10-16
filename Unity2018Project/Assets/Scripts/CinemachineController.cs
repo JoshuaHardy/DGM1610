fd6b68439fed4576b6e598d6a4b6f7c7bab7e0f1
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-10-16 15:53:37 -0600

    10/15/2019 Lecture

---
+++ Unity2018Project/Assets/Scripts/CinemachineController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CinemachineController : MonoBehaviour
+{
+    public PlayerData playerData;
+    private CinemachineVirtualCamera virtualCamera;
+    void Awake()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        
+    }
+
+    private void InstanceHandler(GameObject obj)
+    {
+        virtualCamera.Follow = obj;
+        playerData.instanceAction = InstanceHandler;
+        playerData.InstancePlayer();
+    }
+    
+}
--- Unity2018Project/Assets/Scripts/PlayerData.cs
+++ Unity2018Project/Assets/Scripts/PlayerData.cs
@@ -1,20 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 
 public class PlayerData : GameArtData
 {
-   public FloatData health;
+   public UnityAction<GameObject> instanceAction;
+   
+   
+   public FloatDataScript health;
    public List<WeaponData> weapons;
    public ClothesData shirt;
    public ClothesData pants;
 
-//   public void InstancePlayer()
-//   {
-//      var newPlayer = Instantiate(prefab);
-//      var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
-//      newSprite.sprite = sprite;
-//      newSprite.color = color;
-//   }
+
+   public Void InstancePlayer()
+   {
+      var newPlayer = Instantiate(prefab);
+      var playerSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+      playerSprite.sprite = sprite;
+    //  playerSprite.color = spriteColor;
+      instanceAction(newPlayer);
+   }
+
+   
 }
