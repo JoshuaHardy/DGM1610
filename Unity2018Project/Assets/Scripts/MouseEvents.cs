91391c11008e7cf06d293c619d5ea119e28b5ef6
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-11-05 16:01:27 -0700

    AI Lecture Part2

--- Unity2018Project/Assets/Scripts/MouseEvents.cs
+++ Unity2018Project/Assets/Scripts/MouseEvents.cs
@@ -1,16 +1,34 @@
-﻿using System;
+﻿/*using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.AI;
 
 public class MouseEvents : MonoBehaviour
 {
     public UnityEvent mouseDown;
     public UnityEvent mouseUp;
+    public Vector3 data;
+    public Camera cam;
+    
+    private void Start()
+    {
+        cam = Camera.main;
+    }
     private void OnMouseDown()
     {
         mouseDown.Invoke();
+        RaycastHit hit;
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            data.value = hit.point
+        }        
+        Debug.Log(hit);
     }
 
     private void OnMouseUp()
@@ -18,4 +36,7 @@ public class MouseEvents : MonoBehaviour
         mouseUp.Invoke();
     }
     
-}
+    Onmouserigh
+
+    
+}*/
