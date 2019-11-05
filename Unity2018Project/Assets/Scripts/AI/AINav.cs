dfbec32da2bf360504e92964078bd08d06e64a81
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-11-05 16:01:27 -0700

    AI Lecture Part2

---
+++ Unity2018Project/Assets/Scripts/AI/AINav.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+/*
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+
+public class AINav : MonoBehaviour;
+
+{
+    private NavMeshAgent agent;
+    void Start()
+    { 
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        destination = LocationInfo.value;
+    }
+}*/
