4d71cd9a3944d2ae9c7c905e5959d8b90934b81e
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-11-16 10:55:20 -0700

    TimerWork
    Still not updating the UI.

--- Unity2018Project/Assets/Scripts/Coroutines/TimerCoroutine.cs
+++ Unity2018Project/Assets/Scripts/Coroutines/TimerCoroutine.cs
@@ -2,19 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerCoroutine : MonoBehaviour
 {
     public bool canRun = true;
-    public int minutesTimer = 0, secondsTimer = 0;
+    public int secondsTimer = 0; //minutesTimer = 0;
     private WaitForSeconds timerWFS;
-    public Time = 00.00f;
+    public Text timerText;
+    
 
 
     public void Run()
     {
         StartCoroutine(routine: OnRun());
         timerWFS = new WaitForSeconds(1);
+        
+
     }
     IEnumerator OnRun()
     {
@@ -22,12 +26,21 @@ public class TimerCoroutine : MonoBehaviour
         {
             secondsTimer++;
             yield return timerWFS;
-            if (secondsTimer == 60)
+            if (secondsTimer >= 60)
             {
                 secondsTimer = 0;
-                minutesTimer++;
+                //minutesTimer++;
             }
         }
        
     }
+
+    void Update()
+    {
+        timerText.text = secondsTimer.ToString();
+        //timerText.text = minutesTimer.ToString();
+        //timerText.text = (minutesTimer":"secondsTimer);
+    }
 }
+//https://www.noob-programmer.com/unity3d/countdown-timer/
+//https://answers.unity.com/questions/1273054/change-text-value.html
