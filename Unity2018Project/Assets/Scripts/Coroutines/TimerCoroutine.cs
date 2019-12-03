8b86e7ee3c12350e5d017e57d84b4398cf6df259
Joshua Hardy <joshua.james.hardy@gmail.com>
2019-12-02 21:41:35 -0700

    Targets 2-10 implemented.

--- Unity2018Project/Assets/Scripts/Coroutines/TimerCoroutine.cs
+++ Unity2018Project/Assets/Scripts/Coroutines/TimerCoroutine.cs
@@ -43,7 +43,7 @@ public class TimerCoroutine : MonoBehaviour
 
     IEnumerator OnRun()
     {
-        while (secondsTimer < 5)
+        while (secondsTimer < 60)
         {
             secondsTimer++;
             if (secondsTimer >= 60)
